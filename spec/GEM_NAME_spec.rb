7c4b9c4d699348faa48eb690f59626a5b5c4e0db
grosser <grosser.michael@gmail.com>
2011-09-28 21:10:09 +0200

    more general version rex

--- spec/GEM_NAME_spec.rb
+++ spec/GEM_NAME_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path('spec/spec_helper')
 
 describe GEM_CLASS_NAME do
   it "has a VERSION" do
-    GEM_CLASS_NAME::VERSION.should =~ /^\d+\.\d+\.\d+$/
+    GEM_CLASS_NAME::VERSION.should =~ /^[\.\da-z]+$/
   end
 end
